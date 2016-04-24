8fb64c020608d6b5ef4d96059aabdd5eb96fe19f
unknown <you@example.com>
2016-04-24 14:33:34 +0800

    no message

--- gameengine/lib/main.js
+++ gameengine/lib/main.js
@@ -1,3 +1,37 @@
+"use strict";
+var fs = require('fs');
+var m_Undolength = 0;
+var m_Undo = new Array(4);
+var m_RecordTile = new editor.Tile;
+for (var i = 0; i < 3; i++) {
+    m_Undo[i] = new Array();
+}
+function readFile() {
+    var map_path = __dirname + "/map.json";
+    var content = fs.readFileSync(map_path, "utf-8");
+    var obj = JSON.parse(content);
+    var mapData = obj.map;
+    return mapData;
+}
+function writeFile() {
+    console.log(mapData);
+    var map_path = __dirname + "/map.json";
+    var json = "{\"map\":" + JSON.stringify(mapData) + "}";
+    fs.writeFileSync(map_path, json, "utf-8");
+}
+function UndoTile() {
+    if (m_Undolength <= 0) {
+        alert("Ended");
+        return;
+    }
+    else {
+        var new_row = m_Undo[0][m_Undolength - 1];
+        var new_col = m_Undo[1][m_Undolength - 1];
+        mapData[new_row][new_col] = m_Undo[2][m_Undolength - 1];
+        m_Undolength--;
+        m_RecordTile.setWalkable(mapData[new_row][new_col]);
+    }
+}
 function createMapEditor() {
     var world = new editor.WorldMap();
     var rows = mapData.length;
@@ -18,7 +52,45 @@ function createMapEditor() {
     }
     return world;
 }
+var m_buttonSave = new render.Bitmap();
+m_buttonSave.width = 50;
+m_buttonSave.height = 50;
+m_buttonSave.source = "Save.png";
+m_buttonSave.y = 200;
+var Save = function (localPoint, displayObject) {
+    if (localPoint.x >= 0 && localPoint.x <= m_buttonSave.width && localPoint.y >= 0 && localPoint.y <= m_buttonSave.height)
+        return true;
+};
+function onSaveClick() {
+    writeFile();
+    console.log("Save");
+}
+var m_buttonUndo = new render.Bitmap();
+m_buttonUndo.width = 50;
+m_buttonUndo.height = 50;
+m_buttonUndo.source = "Cannel.png";
+m_buttonUndo.x = 150;
+m_buttonUndo.y = 200;
+var Cannel = function (localPoint, displayObject) {
+    if (localPoint.x >= 0 && localPoint.x <= m_buttonUndo.width && localPoint.y >= 0 && localPoint.y <= m_buttonUndo.height)
+        return true;
+};
+function onCannelClick() {
+    UndoTile();
+    console.log("Cannel");
+}
 function onTileClick(tile) {
+    console.log(tile.ownedRow + " " + tile.ownedCol + " " + mapData[tile.ownedRow][tile.ownedCol]);
+    m_Undo[0][m_Undolength] = tile.ownedRow;
+    m_Undo[1][m_Undolength] = tile.ownedCol;
+    m_Undo[2][m_Undolength] = mapData[tile.ownedRow][tile.ownedCol];
+    m_Undolength++;
+    if (mapData[tile.ownedRow][tile.ownedCol] == 0)
+        mapData[tile.ownedRow][tile.ownedCol] = 1;
+    else
+        mapData[tile.ownedRow][tile.ownedCol] = 0;
+    tile.setWalkable(mapData[tile.ownedRow][tile.ownedCol]);
+    m_RecordTile = tile;
     console.log(tile);
 }
 var storage = data.Storage.getInstance();
@@ -33,4 +105,9 @@ stage.addChild(mapEditor);
 var panel = new editor.ControlPanel();
 panel.x = 300;
 stage.addChild(panel);
+stage.addChild(m_buttonSave);
+stage.addChild(m_buttonUndo);
+renderCore.start(stage, ["Save.png", "Cannel.png"]);
+eventCore.register(m_buttonSave, Save, onSaveClick);
+eventCore.register(m_buttonUndo, Cannel, onCannelClick);
 renderCore.start(stage);
